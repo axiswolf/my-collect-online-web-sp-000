8325c23022e1e84b36cf8df61899c7fee148923c
Karen V <karenvo@usc.edu>
2019-02-25 09:14:31 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,8 @@
 def my_collect(names)
-  if names == languages
     i = 0
     while i < names.length
       names.uppercase[i]
       i += 1
     end
-  names
-  else names.split[i]
-  end
+    names
 end
